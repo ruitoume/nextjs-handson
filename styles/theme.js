195eb2745cde7bdc0ba59cde7a687bcada8587d7
rui <r.toume@churadata.okinawa>
2023-03-01 15:21:25 +0900

    色の変更

--- styles/theme.js
+++ styles/theme.js
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { createTheme } from '@mui/material/styles';
-import { blue, purple } from '@mui/material/colors';
+import { blue, pink } from '@mui/material/colors';
 
 const theme = createTheme({
   palette: {
     primary: blue,
-    secondary: purple,
+    secondary: pink,
   },
 });
 
